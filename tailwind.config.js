c05d7e91f356b0bef24894ea49d36028667aec1f
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2022-07-29 12:56:01 -0400

    +font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +7,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ["IM Fell English", ...defaultTheme.fontFamily.serif]
+      }
+    },
   },
   plugins: [],
 };
